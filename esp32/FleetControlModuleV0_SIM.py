417ee4d4b610b1c20608ffb8a5123ae4554aa12f
rivros0 <rivros@gmail.com>
2024-06-17 19:15:32 +0200

    aggiunti file leafleet

--- esp32/FleetControlModuleV0_SIM.py
+++ esp32/FleetControlModuleV0_SIM.py
@@ -3,21 +3,23 @@ import time
 import random
 
 server_url = 'http://test0000.pythonanywhere.com/api/update_location'
-vehicle_id = 'VEHICLE002'
+vehicle_ids = ['VEHICLE001','VEHICLE002','VEHICLE003']
 
 while True:
     latitude = random.uniform(38.0, 42.0)
     longitude = random.uniform(17.0, 18.0)
     timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
 
-    data = {
-        'vehicle_id': vehicle_id,
-        'latitude': latitude,
-        'longitude': longitude,
-        'timestamp': timestamp
-    }
+    for i in range(len(vehicle_ids)):
+        data = {
+            'vehicle_id': vehicle_ids[i],
+            'latitude': latitude,
+            'longitude': longitude,
+            'timestamp': timestamp
+        }
 
-    response = requests.post(server_url, data=data)
-    print(response.text)
-    print(timestamp)
-    time.sleep(5)
+        response = requests.post(server_url, data=data)
+        print(response.text)
+        print(data)
+        print(timestamp)
+        time.sleep(5)
